c14c5a82108ed711ec0d38936558fd89eb506615
kwunderle <95194045+kwunderle@users.noreply.github.com>
2022-08-16 21:34:31 -0400

    Add files via upload

---
+++ CardIdentifier - Katharine Wunderle/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿
+namespace CardIdentifier___Katharine_Wunderle
+{
+    partial class cardIdentifier
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(cardIdentifier));
+            this.clickacardLabel = new System.Windows.Forms.Label();
+            this.aceofheartsPicture = new System.Windows.Forms.PictureBox();
+            this.eightofspadesPicture = new System.Windows.Forms.PictureBox();
+            this.queenofheartsPicture = new System.Windows.Forms.PictureBox();
+            this.jackofclubsPicture = new System.Windows.Forms.PictureBox();
+            this.sevenofheartsPicture = new System.Windows.Forms.PictureBox();
+            this.cardnameLabel = new System.Windows.Forms.Label();
+            this.exitButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.aceofheartsPicture)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.eightofspadesPicture)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.queenofheartsPicture)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.jackofclubsPicture)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sevenofheartsPicture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // clickacardLabel
+            // 
+            this.clickacardLabel.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.clickacardLabel.AutoSize = true;
+            this.clickacardLabel.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.clickacardLabel.Location = new System.Drawing.Point(67, 9);
+            this.clickacardLabel.Name = "clickacardLabel";
+            this.clickacardLabel.Size = new System.Drawing.Size(143, 13);
+            this.clickacardLabel.TabIndex = 0;
+            this.clickacardLabel.Text = "Click a Card to See Its Name";
+            // 
+            // aceofheartsPicture
+            // 
+            this.aceofheartsPicture.Image = ((System.Drawing.Image)(resources.GetObject("aceofheartsPicture.Image")));
+            this.aceofheartsPicture.Location = new System.Drawing.Point(5, 35);
+            this.aceofheartsPicture.Name = "aceofheartsPicture";
+            this.aceofheartsPicture.Size = new System.Drawing.Size(50, 70);
+            this.aceofheartsPicture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.aceofheartsPicture.TabIndex = 1;
+            this.aceofheartsPicture.TabStop = false;
+            this.aceofheartsPicture.Click += new System.EventHandler(this.aceofheartsPicture_Click);
+            // 
+            // eightofspadesPicture
+            // 
+            this.eightofspadesPicture.Image = ((System.Drawing.Image)(resources.GetObject("eightofspadesPicture.Image")));
+            this.eightofspadesPicture.Location = new System.Drawing.Point(61, 35);
+            this.eightofspadesPicture.Name = "eightofspadesPicture";
+            this.eightofspadesPicture.Size = new System.Drawing.Size(50, 70);
+            this.eightofspadesPicture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.eightofspadesPicture.TabIndex = 2;
+            this.eightofspadesPicture.TabStop = false;
+            this.eightofspadesPicture.Click += new System.EventHandler(this.eightofspadesPicture_Click);
+            // 
+            // queenofheartsPicture
+            // 
+            this.queenofheartsPicture.Image = ((System.Drawing.Image)(resources.GetObject("queenofheartsPicture.Image")));
+            this.queenofheartsPicture.Location = new System.Drawing.Point(117, 35);
+            this.queenofheartsPicture.Name = "queenofheartsPicture";
+            this.queenofheartsPicture.Size = new System.Drawing.Size(50, 70);
+            this.queenofheartsPicture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.queenofheartsPicture.TabIndex = 3;
+            this.queenofheartsPicture.TabStop = false;
+            this.queenofheartsPicture.Click += new System.EventHandler(this.queenofheartsPicture_Click);
+            // 
+            // jackofclubsPicture
+            // 
+            this.jackofclubsPicture.Image = ((System.Drawing.Image)(resources.GetObject("jackofclubsPicture.Image")));
+            this.jackofclubsPicture.Location = new System.Drawing.Point(174, 35);
+            this.jackofclubsPicture.Name = "jackofclubsPicture";
+            this.jackofclubsPicture.Size = new System.Drawing.Size(50, 70);
+            this.jackofclubsPicture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.jackofclubsPicture.TabIndex = 4;
+            this.jackofclubsPicture.TabStop = false;
+            this.jackofclubsPicture.Click += new System.EventHandler(this.jackofclubsPicture_Click);
+            // 
+            // sevenofheartsPicture
+            // 
+            this.sevenofheartsPicture.Image = ((System.Drawing.Image)(resources.GetObject("sevenofheartsPicture.Image")));
+            this.sevenofheartsPicture.Location = new System.Drawing.Point(231, 35);
+            this.sevenofheartsPicture.Name = "sevenofheartsPicture";
+            this.sevenofheartsPicture.Size = new System.Drawing.Size(50, 70);
+            this.sevenofheartsPicture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.sevenofheartsPicture.TabIndex = 5;
+            this.sevenofheartsPicture.TabStop = false;
+            this.sevenofheartsPicture.Click += new System.EventHandler(this.sevenofheartsPicture_Click);
+            // 
+            // cardnameLabel
+            // 
+            this.cardnameLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.cardnameLabel.Location = new System.Drawing.Point(5, 117);
+            this.cardnameLabel.Name = "cardnameLabel";
+            this.cardnameLabel.Size = new System.Drawing.Size(276, 23);
+            this.cardnameLabel.TabIndex = 6;
+            this.cardnameLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(105, 148);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 23);
+            this.exitButton.TabIndex = 7;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // cardIdentifier
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(287, 179);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.cardnameLabel);
+            this.Controls.Add(this.sevenofheartsPicture);
+            this.Controls.Add(this.jackofclubsPicture);
+            this.Controls.Add(this.queenofheartsPicture);
+            this.Controls.Add(this.eightofspadesPicture);
+            this.Controls.Add(this.aceofheartsPicture);
+            this.Controls.Add(this.clickacardLabel);
+            this.Name = "cardIdentifier";
+            this.Text = "Card Identifier";
+            ((System.ComponentModel.ISupportInitialize)(this.aceofheartsPicture)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.eightofspadesPicture)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.queenofheartsPicture)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.jackofclubsPicture)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sevenofheartsPicture)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label clickacardLabel;
+        private System.Windows.Forms.PictureBox aceofheartsPicture;
+        private System.Windows.Forms.PictureBox eightofspadesPicture;
+        private System.Windows.Forms.PictureBox queenofheartsPicture;
+        private System.Windows.Forms.PictureBox jackofclubsPicture;
+        private System.Windows.Forms.PictureBox sevenofheartsPicture;
+        private System.Windows.Forms.Label cardnameLabel;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
+
---
+++ CardIdentifier - Katharine Wunderle/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CardIdentifier___Katharine_Wunderle
+{
+    //Author: Katharine Wunderle
+    //ID: 623748
+    //Date: 02/08/2021
+    //Goal: To identify a card by name when the image is clicked on.
+    public partial class cardIdentifier : Form
+    {
+        public cardIdentifier()
+        {
+            InitializeComponent();
+        }
+
+        private void aceofheartsPicture_Click(object sender, EventArgs e)
+        {
+            //Display Ace of Hearts in cardnameLabel when image is clicked.
+            cardnameLabel.Text = "Ace of Hearts";
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            //Exits out of program.
+            this.Close();
+        }
+
+        private void eightofspadesPicture_Click(object sender, EventArgs e)
+        {
+            //Display Eight of Spades in cardnameLabel when image is clicked.
+            cardnameLabel.Text = "Eight of Spades";
+        }
+
+        private void queenofheartsPicture_Click(object sender, EventArgs e)
+        {
+            //Display Queen of Hearts in cardnameLabel when image is clicked.
+            cardnameLabel.Text = "Queen of Hearts";
+        }
+
+        private void jackofclubsPicture_Click(object sender, EventArgs e)
+        {
+            //Display Jack of Clubs in cardnameLabel when image is clicked.
+            cardnameLabel.Text = "Jack of Clubs";
+        }
+
+        private void sevenofheartsPicture_Click(object sender, EventArgs e)
+        {
+            //Display Seven of Hearts in cardnameLabel when image is clicked.
+            cardnameLabel.Text = "Seven of Hearts";
+        }
+    }
+}
